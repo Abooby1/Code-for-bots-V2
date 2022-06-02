fa2e1cd5b7424faf23803023555f7d54aa748593
Abooby <97899737+Abooby1@users.noreply.github.com>
2022-06-02 15:35:53 +0000

    Added data & more to commands!

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 const {Client} = require('photop-client')
 
-const client = new Client({username: 'username', password: 'password'})
+const client = new Client({username: 'Abicam_Engine', password: process.env['Pass']})
 
-const START = '' //the thing someone has to post to connect the bot
-const STARTTEXT = '' //the chat the bot sends when someone connects a post
-const TIME = 0 //the time (in milliseconds) that the post is connected until disconnected because of inactivity
-const PREFIX = ''//the prefix of the bot (no spaces)
-const WrongCommand = ''//the text when someone does a command that does not exist
+const JSONdb = require('simple-json-db')
+const db = new JSONdb('storage.json')
+
+const START = 'testing' //the thing someone has to post to connect the bot
+const STARTTEXT = 'test' //the chat the bot sends when someone connects a post
+const TIME = 60000 //the time (in milliseconds) that the post is connected until disconnected because of inactivity
+const PREFIX = 'test!'//the prefix of the bot (no spaces)
+const WrongCommand = 'hey'//the text when someone does a command that does not exist
 
 function getCom (c) {//all the commands
   switch (c) {
     case 'test'://command name
       return {
         name: 'test',//name of the command (again so it doesnt repeat the command name instead of the body)
-        func: ({ chat, body, args: [h] }) => {//add either chat, post, client, or body
-          chat.reply(`hi ${body} ${h}`)//code for the command
+        func: ({ chat, body, args: [h], data }) => {//add either chat, client, data, args, or body
+          chat.reply(`hi ${body} ${h}... ${data.lvl}`)//code for the command
         }
       }
+  }
+}
+
+const Version = 1//the version of the data (to update data)
+
+const defaultData = {//the data that a new user gets
+  version: 1
+}
+
+async function VersionUpdate (uid) {//for updating data (for users that have already used the bot)
+  const d = JSON.parse(await db.get(uid))
+  switch (d.version) {
+    case 1://the version before the data update
+      d.lvl = 1
+      db.set(uid, JSON.stringify(d))
+      break;
   }
 }
 
@@ -33,7 +52,7 @@ client.onPost = async (post) => {
       }
     })
     resettime()
-    post.onChat = (chat) => {
+    post.onChat = async (chat) => {
       resettime()
       if (chat.text.toLowerCase().startsWith(PREFIX)) {
         const match = chat.text.substring(PREFIX.length).match(/([a-z0-9\.]+)(.*)/i);
@@ -42,6 +61,15 @@ client.onPost = async (post) => {
           const body = _body.trim()
           const args = body.split(/\s+/);
           const context = { client, chat, body, args }
+          if (db.get(chat.author.id) != undefined) {
+            context.data = JSON.parse(await db.get(chat.author.id))
+            if (context.data.version != Version) {
+              VersionUpdate(chat.author.id)
+            }
+          } else {
+            db.set(chat.author.id, JSON.stringify(defaultData))
+            context.data = defaultData
+          }
           setTimeout(async function( ) {
             if (getCom(commandname) != undefined) {
               getCom(commandname).func(context)
